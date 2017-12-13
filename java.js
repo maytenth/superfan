4cd9aeb1860493169a22f990b959261bedac31b3
maytenth <lane_butler@davincischools.org>
2017-12-13 09:18:15 -0800

    Create java.js

---
+++ java.js
@@ -0,0 +1,12 @@
+function showTime(elem){
+	elem.classList.toggle("one");
+}
+function showYear(elem){
+	elem.classList.toggle("two");
+}
+function showTimeless(elem){
+	elem.classList.toggle("timeless");
+}
+function playSound(e){
+	document.getElementById("oprah").play();
+}
